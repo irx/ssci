77fe6242660b342bb35b35005edbe4f39efa7a73
MM <u@1ux.org>
2019-03-22 19:46:39 +0100

    Init
    It works, but it's still more of a draft...

---
+++ client.c
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2019  Maksymilian Mruszczak <u at one u x dot o r g>
+ *
+ * Simplified sockets client-side API implementation
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <poll.h>
+
+enum {
+	ON_MESSG,
+	ON_CLOSE,
+	ON_STDIN
+};
+
+typedef struct Conn Conn;
+struct Conn {
+	struct sockaddr_in addr;
+	struct pollfd pfd[2];
+	void (*on_messg)(const Conn *, const char *, const unsigned int);
+	void (*on_close)(const Conn *);
+	void (*on_stdin)(const Conn *, const char *, const unsigned int);
+};
+
+Conn *
+client_dial(const char *saddr, const unsigned int port)
+{
+	static socklen_t slen = (socklen_t)sizeof(struct sockaddr_in);
+
+	Conn *conn = (Conn *)calloc(1, sizeof(Conn));
+	if (conn < 0)
+		return conn;
+	conn->on_messg = NULL;
+	conn->on_close = NULL;
+	conn->on_stdin = NULL;
+
+	conn->pfd[0].fd = -1;
+	conn->pfd[0].events = POLLIN;
+	conn->pfd[1].fd = socket(AF_INET, SOCK_STREAM, 0);
+	conn->pfd[1].events = POLLIN;
+	if (conn->pfd[1].fd < 0) {
+		perror("Failed to create socket!");
+		free(conn);
+		return NULL;
+	}
+
+	conn->addr.sin_family = AF_INET;
+	conn->addr.sin_addr.s_addr = inet_addr(saddr);
+	conn->addr.sin_port = htons(port);
+
+	if (connect(conn->pfd[1].fd, (struct sockaddr *)&conn->addr, slen) < 0) {
+		perror("Failed to connex!");
+		free(conn);
+		return NULL;
+	}
+	fcntl(conn->pfd[1].fd, F_SETFL, O_NONBLOCK);
+
+	return conn;
+}
+
+void
+client_poll(Conn *conn, int timeout)
+{
+	poll(conn->pfd, 2, timeout);
+	if ((conn->pfd[0].revents & POLLIN) && conn->on_stdin != NULL) {
+		static char buf[1024]; // tmp
+		bzero((void *)buf, 1024);
+		read(conn->pfd[0].fd, buf, 1024);
+		(*conn->on_stdin)(conn, buf, 1024);
+		// TODO handle stdin
+	}
+	if ((conn->pfd[1].revents & POLLIN) && conn->on_messg != NULL) {
+		static char buf[1024]; // tmp
+		bzero((void *)buf, 1024);
+		if (recv(conn->pfd[1].fd, buf, 1024, 0))
+			(*conn->on_messg)(conn, buf, 1024);
+		else {
+			perror("Lost connexion with server");
+			conn->pfd[1].fd = -1;
+			if (conn->on_close != NULL)
+				(*conn->on_close)(conn);
+		}
+	}
+}
+
+void
+client_send(const Conn *conn, const char *msg, const unsigned int len)
+{
+	// TODO maybe some checks
+	if (len > 1024) {
+		perror("Message too long");
+		return;
+	}
+	send(conn->pfd[1].fd, msg, len, 0);
+}
+
+void
+client_bind(Conn *conn, int event, const void *fn)
+{
+	switch (event) {
+	case ON_MESSG:
+		conn->on_messg = fn;
+		break;
+	case ON_CLOSE:
+		conn->on_close = fn;
+		break;
+	case ON_STDIN:
+		conn->on_stdin = fn;
+		if (fn == NULL)
+			conn->pfd[0].fd = -1;
+		else
+			conn->pfd[0].fd = fileno(stdin);
+		break;
+	}
+}
+
+void
+client_close(Conn *conn)
+{
+	close(conn->pfd[1].fd);
+	bzero((void *)conn, sizeof(Conn));
+	free(conn);
+}
+
+int
+client_alive(Conn *conn)
+{
+	if (!conn)
+		return 0;
+	if (conn->pfd[1].fd < 0)
+		return 0;
+	return 1;
+}
---
+++ server.c
@@ -0,0 +1,225 @@
+/*
+ * Copyright (c) 2019  Maksymilian Mruszczak <u at one u x dot o r g>
+ *
+ * Simplified sockets server-side API implementation
+ */
+
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/socket.h>
+
+enum {
+	ON_MESSG,
+	ON_CLOSE,
+	ON_STDIN
+};
+
+typedef struct {
+	int occupied;
+	size_t pfdno;
+	int idk;
+} Slot;
+
+typedef struct Server Server;
+struct Server {
+	unsigned int limit;
+	Slot *slots;
+	struct pollfd *pfd;
+	struct sockaddr_in *addr;
+	void (*on_messg)(Server *, const unsigned int, const char *, const unsigned int);
+	void (*on_close)(Server *, const unsigned int);
+	void (*on_stdin)(Server *, const char *, const unsigned int);
+};
+
+Server *
+server_open(unsigned int slot_lim, const unsigned int port)
+{
+	const int ok = 1;
+	static socklen_t slen = (socklen_t)sizeof(struct sockaddr_in);
+
+	Server *serv = (Server *)malloc(sizeof(Server));
+	if (serv < 0)
+		return serv;
+	serv->on_messg = NULL;
+	serv->on_close = NULL;
+	serv->on_stdin = NULL;
+	slot_lim += 2;
+	serv->limit = slot_lim;
+
+	serv->slots = (Slot *)malloc(sizeof(Slot)*slot_lim);
+	if (serv->slots < 0) {
+		free(serv);
+		return NULL;
+	}
+	serv->pfd = (struct pollfd *)calloc(1, sizeof(struct pollfd)*slot_lim);
+	if (serv->pfd < 0) {
+		free(serv->slots);
+		free(serv);
+		return NULL;
+	}
+	serv->addr = (struct sockaddr_in *)calloc(1, sizeof(struct sockaddr_in)*slot_lim);
+	if (serv->pfd < 0) {
+		free(serv->pfd);
+		free(serv->slots);
+		free(serv);
+		return NULL;
+	}
+
+	for (int i=0; i < slot_lim; ++i) {
+		serv->pfd[i].fd = -1;
+		serv->pfd[i].events = POLLIN;
+		serv->slots[i].occupied = 0;
+	}
+
+	serv->pfd[0].fd = fileno(stdin);
+	serv->pfd[1].fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (serv->pfd[1].fd < 0) {
+		perror("Error creating new conn socket");
+		free(serv->addr);
+		free(serv->pfd);
+		free(serv->slots);
+		free(serv);
+		return NULL;
+	}
+	setsockopt(serv->pfd[1].fd, SOL_SOCKET, SO_REUSEADDR, &ok, sizeof(ok));
+	serv->addr[1].sin_family = AF_INET;
+	serv->addr[1].sin_addr.s_addr = INADDR_ANY;
+	serv->addr[1].sin_port = htons(port);
+	if (bind(serv->pfd[1].fd, (struct sockaddr *)&(serv->addr[1]), slen) < 0) {
+		perror("Failed to open new conn socket");
+		free(serv->addr);
+		free(serv->pfd);
+		free(serv->slots);
+		free(serv);
+		return NULL;
+	}
+	fcntl(serv->pfd[1].fd, F_SETFL, O_NONBLOCK);
+	listen(serv->pfd[1].fd, 5);
+
+	return serv;
+}
+
+void
+server_abort(Server *serv, const unsigned int clino)
+{
+	if (clino > serv->limit || !serv->slots[clino].occupied)
+		return;
+	close(serv->pfd[clino].fd);
+	serv->slots[clino].occupied = 0;
+	serv->pfd[clino].fd = -1;
+	if (serv->on_close != NULL)
+		(*serv->on_close)(serv, clino);
+}
+
+void
+server_poll(Server *serv, int timeout)
+{
+	static size_t clino;
+	poll(serv->pfd, serv->limit, timeout);
+	if ((serv->pfd[0].revents & POLLIN) && serv->on_stdin != NULL) {
+		static char buf[1024]; // tmp
+		bzero((void *)buf, 1024);
+		read(serv->pfd[0].fd, buf, 1024);
+		(*serv->on_stdin)(serv, buf, 1024);
+		// TODO handle stdin
+	}
+	if (serv->pfd[1].revents & POLLIN) {
+		/* Incoming new connexion */
+		static socklen_t slen = (socklen_t)sizeof(serv->addr[0]);
+		for (clino=2; (clino < serv->limit && serv->slots[clino].occupied); ++clino);
+		if (clino >= serv->limit) {
+			perror("Reached slot limit");
+			// TODO tell client to GTFO
+		} else {
+			serv->pfd[clino].fd = accept(serv->pfd[1].fd, (struct sockaddr *)&(serv->addr[clino]), &slen);
+			if (serv->pfd[clino].fd < 0)
+				perror("Failed to accept new client connexion");
+			else {
+				serv->slots[clino].occupied = 1;
+				fcntl(serv->pfd[clino].fd, F_SETFL, O_NONBLOCK);
+			}
+		}
+	}
+	for (clino=2; clino < serv->limit; ++clino) {
+		if (serv->pfd[clino].revents & POLLIN) {
+			char buf[256]; // tmp
+			bzero((void *)buf, sizeof(char)*256);
+			int r = recv(serv->pfd[clino].fd, buf, 256, 0);
+			if (r == 0) {
+				close(serv->pfd[clino].fd);
+				serv->slots[clino].occupied = 0;
+				serv->pfd[clino].fd = -1;
+				if (serv->on_close != NULL)
+					(*serv->on_close)(serv, clino);
+			} else if (r < 0) {
+				perror("Unexpected response");
+				/* What do I do? */
+			} else {
+				/* Handle message */
+				if (r > 256) r = 256;
+				if (serv->on_messg != NULL)
+					(*serv->on_messg)(serv, clino, buf, r);
+			}
+		}
+	}
+}
+
+void
+server_send(const Server *serv, const unsigned int clino, const char *msg, const unsigned int len)
+{
+	if (clino > serv->limit || !serv->slots[clino].occupied) {
+		perror("No client on given socket");
+		return;
+	}
+	if (len > 1024) {
+		perror("Message too long");
+		return;
+	}
+	send(serv->pfd[clino].fd, msg, len, 0);
+}
+
+void server_broadcast(const Server *serv, const char *msg, const unsigned int len)
+{
+	int clino;
+	for (clino=2; clino < serv->limit; ++clino) {
+		if (serv->slots[clino].occupied)
+			server_send(serv, clino, msg, len);
+	}
+}
+
+void
+server_bind(Server *serv, int event, const void *fn)
+{
+	switch (event) {
+	case ON_MESSG:
+		serv->on_messg = fn;
+		break;
+	case ON_CLOSE:
+		serv->on_close = fn;
+		break;
+	case ON_STDIN:
+		serv->on_stdin = fn;
+		break;
+	}
+}
+
+void
+server_close(Server *serv)
+{
+	for (int i=1; i < serv->limit; ++i)
+		if (serv->pfd[i].fd >= 0)
+			close(serv->pfd[i].fd);
+	bzero((void *)serv->addr, sizeof(serv->addr));
+	bzero((void *)serv->slots, sizeof(serv->slots));
+	free(serv->addr);
+	free(serv->pfd);
+	free(serv->slots);
+	free(serv);
+}
